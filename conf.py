d809a9aa27725ee077e7ace2271ebc55c0dfee7e
vikashkb <vikash.kumar.banjare@intel.com>
2023-07-05 09:07:58 +0530

    Add tmux conf file and script to configure it

--- conf.py
+++ conf.py
@@ -3,5 +3,9 @@ import subprocess
 subprocess.run(['cp', '.vimrc', '~/'])
 subprocess.run(['git', 'clone', 'https://github.com/VundleVim/Vundle.vim.git', '~/.vim/bundle/Vundle.vim'])
 subprocess.run(['vim', '+PluginInstall', '+qa!'])
+
 subprocess.run(['cp', '.bashrc', '~/'])
 subprocess.run(['source', '~/.bashrc'])
+
+subprocess.run(['cp', '.tmux.conf', '~/'])
+subprocess.run(['tmux', 'source-file', '.tmux.conf'])
