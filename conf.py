80a8f8d5ccbb0b5ed636c067c989ac37d4d06b48
vikashkb <vikash.kumar.banjare@intel.com>
2023-07-05 08:56:10 +0530

    Add script to copy bashrc file to home dir

--- conf.py
+++ conf.py
@@ -1,6 +1,7 @@
 import subprocess
 
-subprocess.run(['cp', '.vimrc', '~/.'])
+subprocess.run(['cp', '.vimrc', '~/'])
 subprocess.run(['git', 'clone', 'https://github.com/VundleVim/Vundle.vim.git', '~/.vim/bundle/Vundle.vim'])
 subprocess.run(['vim', '+PluginInstall', '+qa!'])
-subprocess.run(['source', '.bashrc'])
+subprocess.run(['cp', '.bashrc', '~/'])
+subprocess.run(['source', '~/.bashrc'])
