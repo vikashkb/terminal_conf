901e0412aaca36ad02924d3b70d017e6d68f9597
vikashkb <vikash.kumar.banjare@intel.com>
2023-07-05 08:14:51 +0530

    Minor change for vimrc copy script

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.run(['mv', '.vimrc', '~/.'])
+subprocess.run(['cp', '.vimrc', '~/.'])
 subprocess.run(['git', 'clone', 'https://github.com/VundleVim/Vundle.vim.git', '~/.vim/bundle/Vundle.vim'])
 subprocess.run(['vim', '+PluginInstall', '+qa!'])
 subprocess.run(['source', '.bashrc'])
